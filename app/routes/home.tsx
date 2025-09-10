eccbcc85062e9291b153c941fabc51ebb6863157
CheeseyHub <muhammedibrahim613@gmail.com>
2025-09-10 07:21:06 +0500

    feat: added index.d.ts

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import   {resumes} from "../../constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,12 +9,21 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return (<main>
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track your Resume Ratings</h1>
-        <h2>Get your resume and applications rated.</h2>
-      </div>
-    </section>
-  </main>)
+  return (
+    <main>
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Resume Ratings</h1>
+          <h2>Get your resume and applications rated.</h2>
+        </div>
+      </section>
+      {
+        resumes.map((resume) =>(
+          <div>
+            <h1>{resume.jobTitle}</h1>
+          </div>
+        ))
+      }
+    </main>
+  );
 }
