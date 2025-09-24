333c3064b7f0b319e1f9f033102beb27810a3a38
CheeseyHub <muhammedibrahim613@gmail.com>
2025-09-24 17:45:22 +0500

    todo: make the file uploader component

--- app/routes/Upload.tsx
+++ app/routes/Upload.tsx
@@ -1,13 +1,31 @@
 import NavBar from "~/components/NavBar";
+import { useState, type FormEvent } from "react";
+import { stat } from "node:fs/promises";
+
 const Upload = () => {
+  const [isProccessing, setIsProccessing] = useState(false);
+  const [statusText, setStatusText] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {};
+
   return (
     <>
       <NavBar />
       <main className="bg-[url('images/bg-main.svg')] bg-cover">
-        <section className="main-section">
+        <section className="main-section ">
           <div className="page-heading">
-            <h1>Smart feedback for your dream job.</h1>
-            <h2>Drop your resume here for an ATS score improvement tips.</h2>
+            <h1>
+              Smart feedback <br />
+              For your dream job.
+            </h1>
+            {isProccessing ? (
+              <>
+                <h2>{statusText}</h2>
+                <img src="images/resume-scan.gif" alt="Loading..." />
+              </>
+            ) : (
+              <h2>Drop your resume here for an ATS score improvement tips.</h2>
+            )}
           </div>
 
           <form>
@@ -25,8 +43,18 @@ const Upload = () => {
                 id="jobDescription"
                 name="jobDescription"
                 placeholder="Write a clear & concise job description with responsibilites and expectations."
+                rows={4}
               />
             </div>
+
+            <div className="form-div">
+              <label htmlFor="file_uploader">Upload Resume:</label>
+              
+            </div>
+
+            <button type="submit" className="primary-button">
+              Submit
+            </button>
           </form>
         </section>
       </main>
