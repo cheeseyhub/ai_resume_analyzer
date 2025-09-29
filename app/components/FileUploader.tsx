68b00af6f24f00c865873f2ba943b592df986002
CheeseyHub <muhammedibrahim613@gmail.com>
2025-09-29 21:53:21 +0500

    feat : centered icon

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,20 +1,38 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { useState } from "react";
 
-export default function FileUploader() {
+interface FileUploaderProps {
+  onFileSelect: (file: File | null) => void;
+}
+
+export default function FileUploader({ onFileSelect }: FileUploaderProps) {
+  const [file, setFile] = useState();
   const onDrop = useCallback((acceptedFiles) => {
-    console.log("Something is happening ")
+    console.log("Something is happening ");
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className="w-full flex justify-center">
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+      <div className="space-y-4 cursor-pointer">
+        <div className="mx-auto w-16 h-16 flex items-center justifiy-content">
+          <img src="/icons/info.svg" alt="upload" className="size-20" />
+        </div>
+
+        {file ? (
+          <div></div>
+        ) : (
+          <div>
+            <p className="text-lg text-gray-500  flex flex-col items-center">
+              Drag and drop your file here
+              <span className="font-semibold">or click to select</span>
+            </p>
+            <p className="text-lg text-gray-50">PDF (max 20 MB)</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
